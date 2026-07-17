cc5cfcb119385e5e8bdb3908f69fdceb3d29aa80
Elyk <aldrinkyles1219@gmail.com>
2026-07-17 14:55:45 +0800

    Indexing

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,8 @@ CREATE TABLE IF NOT EXISTS books (
     genre VARCHAR(50) NOT NULL,
     price DECIMAL(10,2) NOT NULL
     );
+
+CREATE INDEX idx_books_title ON books(title);
+CREATE INDEX idx_books_author ON books(author);
+CREATE INDEX idx_books_genre ON books(genre);
+CREATE INDEX idx_books_price ON books(price);

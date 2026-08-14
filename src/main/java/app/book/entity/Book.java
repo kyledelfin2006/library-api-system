b6db055e1da6caac82ee21f7bff18614ec3f936e
Elyk <aldrinkyles1219@gmail.com>
2026-08-15 01:05:35 +0800

    [chore] boilerplate

--- src/main/java/app/book/entity/Book.java
+++ src/main/java/app/book/entity/Book.java
@@ -5,6 +5,9 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
 /**
@@ -15,6 +18,7 @@ import java.math.BigDecimal;
  * The JSON serialization order is defined by {@link JsonPropertyOrder}.
  * </p>
  */
+@Getter @Setter
 @Entity
 @Table(name = "books")
 @JsonPropertyOrder({"id", "title", "author", "genre", "price"})

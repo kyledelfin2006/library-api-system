8f5f917d30ccdb565a032a8ee0bf0593a1bb0b89
Elyk <aldrinkyles1219@gmail.com>
2026-08-15 01:11:09 +0800

    [chore] boilerplate

--- src/main/java/app/book/dto/BookRequestDTO.java
+++ src/main/java/app/book/dto/BookRequestDTO.java
@@ -5,9 +5,16 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BookRequestDTO {
 
@@ -28,27 +35,4 @@ public class BookRequestDTO {
     @NotNull(message = "Price is required")
     @Positive(message = "Price must be greater than 0")
     private BigDecimal price;
-
-    // No-arg constructor for Jackson deserialization
-    public BookRequestDTO() {}
-
-    // Clean constructor without inline validation annotations
-    public BookRequestDTO(String title, String author, String genre, BigDecimal price) {
-        this.title = title;
-        this.author = author;
-        this.genre = genre;
-        this.price = price;
-    }
-
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public String getAuthor() { return author; }
-    public void setAuthor(String author) { this.author = author; }
-
-    public String getGenre() { return genre; }
-    public void setGenre(String genre) { this.genre = genre; }
-
-    public BigDecimal getPrice() { return price; }
-    public void setPrice(BigDecimal price) { this.price = price; }
 }

b4cf7d16f15e31990c414190a117cbdcb7de7796
Elyk <aldrinkyles1219@gmail.com>
2026-09-03 02:08:12 +0800

    [chore] lombok error response

--- src/main/java/app/global/responses/ErrorResponse.java
+++ src/main/java/app/global/responses/ErrorResponse.java
@@ -1,11 +1,13 @@
 package app.global.responses;
 
+import lombok.Getter;
+
+@Getter
 public class ErrorResponse {
     private final String error;
     private final String details;
     private final long timestamp;
     private final int statusCode;
-
 
     public ErrorResponse(String error, String details, int statusCode) {
         this.error = error;
@@ -14,17 +16,4 @@ public class ErrorResponse {
         this.statusCode = statusCode;
     }
 
-
-    public int getStatusCode() {
-        return statusCode;
-    }
-    public String getError() {
-        return error;
-    }
-    public String getDetails() {
-        return details;
-    }
-    public long getTimestamp() {
-        return timestamp;
-    }
 }
